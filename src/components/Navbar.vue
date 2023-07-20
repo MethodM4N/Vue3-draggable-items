3b90fe4269226fb757131684a853631ac44c84af
Alexandr Kolganov <gengizar@yandex.ru>
2023-07-20 14:46:43 +0500

    feat(Navbar): add skeleton animation

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,6 +12,16 @@ export default {
 </template>
 
 <style scoped lang="scss">
+@keyframes skeleton-animation {
+  0% {
+    transform: translateX(0);
+  }
+
+  100% {
+    transform: translateX(110px);
+  }
+}
+
 .navbar {
   position: relative;
   display: flex;
@@ -39,11 +49,31 @@ export default {
   }
 
   div {
+    position: relative;
     width: 190px;
     height: 26px;
     border-radius: 8px;
     margin-bottom: 172px;
-    background: linear-gradient(0.25turn, #3c3c3c 0%, #444444 50%, #3c3c3c 100%);
+    background-color: #3c3c3c;
+
+    &:after {
+      content: '';
+      position: absolute;
+      width: 80px;
+      height: 100%;
+      top: 0;
+      left: 0;
+      right: 0;
+      background: linear-gradient(
+        90deg,
+        rgba(227, 227, 227, 0) 0%,
+        #444444b7 30%,
+        #444 50%,
+        #444444b7 70%,
+        rgba(227, 227, 227, 0) 100%
+      );
+      animation: skeleton-animation 1.2s linear infinite alternate;
+    }
   }
 }
 </style>
